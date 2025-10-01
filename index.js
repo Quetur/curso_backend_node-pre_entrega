374d1f953ab386d8fae037083841edc3569aaa29
quetur <jrosavila@gmail.com>
2025-10-01 20:00:32 -0300

    creo proyecto

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import { obtenerProductos } from "./funciones/funciones.js";
+
+const argumentos = process.argv.slice(2);
+console.log(argumentos);
+
+if (argumentos[0] == "GET" && argumentos[1] == "products") { // con == es el contenido con === es el contenido y el tipo de dato
+    console.log("obtener productos")
+    obtenerProductos()  
+}
+else if (argumentos[0] == "GET" && argumentos[1].includes("/products/")) {
+    obtenerProductos(argumentos[1])
+} 
+else if (argumentos[0] == "POST" && argumentos[1].lenght>0 && argumentos[2].lenght > 0 && argumentos[3].lenght > 0 ){
+    console.log("comando no reconocido")
+}
