e21620f9b6b233be7972e2438ccdfea91fa986b4
quetur <jrosavila@gmail.com>
2025-10-02 17:31:30 -0300

    cambio en las funciones

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-import { obtenerProductos } from "./funciones/funciones.js";
+import { obtenerProductos,obtenerProducto, agregarProducto , eliminarProducto} from "./funciones/funciones.js";
 
 const argumentos = process.argv.slice(2);
-console.log(argumentos);
+console.log("argumentos",argumentos);
+console.log("Argumentos 1",argumentos[1]);
+console.log("largo de argumento 1 = ",argumentos.length)
 
 if (argumentos[0] == "GET" && argumentos[1] == "products") { // con == es el contenido con === es el contenido y el tipo de dato
-    console.log("obtener productos")
+    console.log("Entro en obtener productos")
+
     obtenerProductos()  
 }
-else if (argumentos[0] == "GET" && argumentos[1].includes("/products/")) {
-    obtenerProductos(argumentos[1])
+else if (argumentos[0] == "GET" && argumentos[1].includes("products/")) {
+    console.log("obtener producto por id", argumentos[1].substr(9,2))
+    obtenerProducto( argumentos[1].substr(9,2))
 } 
-else if (argumentos[0] == "POST" && argumentos[1].lenght>0 && argumentos[2].lenght > 0 && argumentos[3].lenght > 0 ){
+else if (argumentos[0] == "POST" && argumentos[1] == "products" && argumentos.length == 5 ){
+    console.log("agregar productos")
+    const producto = {
+        title: argumentos[2],
+        price: argumentos[3],
+        category: argumentos[4]}
+    agregarProducto(producto)
+}
+else if (argumentos[0] == "DELETE" && argumentos[1].includes("product/") ){
+    const id = argumentos[1].substr(8,2)
+    console.log("eliminar dentro del if id : ", id)
+    eliminarProducto(id)
+}
+else {
     console.log("comando no reconocido")
 }
+
